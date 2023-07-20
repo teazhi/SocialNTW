b8a4a9d461aa70713632cb46d6ac83321cfd90ce
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2023-07-19 21:01:10 -0400

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -4,13 +4,17 @@ const mongoose = require('mongoose');
 const connectDatabase = () => {
     console.log("Connecting DB..")
     console.log(process.env.DB_URI)
-    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('Connected to database');
-    })
-    .catch((error) => {
-        console.log('Error connecting to MangoDB database: ', error.message);
-    });
+    mongoose.connect(process.env.DB_URI, null, (error => {
+        error ?
+            console.log(error) :
+            console.log("Website Traffic database connected.");
+    }));
+    // .then(() => {
+    //     console.log('Connected to database');
+    // })
+    // .catch((error) => {
+    //     console.log('Error connecting to MangoDB database: ', error.message);
+    // });
 };
 
 // Export the connectDatabase function to be used in other files
