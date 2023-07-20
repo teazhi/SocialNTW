5bef2500460f8e8133b1b1cac03646d5445d00be
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2023-07-19 23:40:29 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 3000;
 
 (async () => {
     try {
-        console.log('Testing')
         await connectDatabase();
     } catch (error) {
         console.error('Failed to connect to the database', error.message);
