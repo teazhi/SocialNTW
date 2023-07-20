d326f5e62545aae99e603de41c303ebb5e01f043
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2023-07-19 20:51:47 -0400

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 // Establish connection with MongoDB Atlas database
 const connectDatabase = () => {
+    console.log("Connecting DB..")
+    console.log(process.env.DB_URI)
     mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to database');
