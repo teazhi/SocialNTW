9376a95618ec677acb757265e7a0e7d36d18b312
Tevin Zhuo <73798707+teazhi@users.noreply.github.com>
2023-07-19 21:15:28 -0400

    .

--- backend/database.js
+++ backend/database.js
@@ -1,21 +1,12 @@
 const mongoose = require('mongoose');
 
-// Establish connection with MongoDB Atlas database
-const connectDatabase = () => {
-    console.log("Connecting DB..")
-    console.log(process.env.DB_URI)
-    mongoose.connect(process.env.DB_URI, null, (error => {
-        error ?
-            console.log(error) :
-            console.log("Website Traffic database connected.");
-    }));
-    // .then(() => {
-    //     console.log('Connected to database');
-    // })
-    // .catch((error) => {
-    //     console.log('Error connecting to MangoDB database: ', error.message);
-    // });
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('Connected to database');
+    } catch (error) {
+        console.log('Error connecting to MongoDB database: ', error.message);
+    }
 };
 
-// Export the connectDatabase function to be used in other files
 module.exports = connectDatabase;
